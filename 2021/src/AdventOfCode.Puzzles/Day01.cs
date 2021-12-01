30c2a5137b031006ea5f9eaec25f2a3a33f2e80f
Dick van Hirtum <dick@xprtz.net>
2021-12-01 13:21:51 +0100

    day 01

---
+++ 2021/src/AdventOfCode.Puzzles/Day01.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day01
+    {
+        public int Solve1(int[] input) =>
+            input
+                .Skip(1)
+                .Zip(input, (current, previous) => current > previous ? 1 : 0)
+                .Sum();
+
+        public int Solve2(int[] input)
+        {
+            var sliding = input
+                .Skip(2)
+                .Zip(input.Skip(1), (third, second) => second + third)
+                .Zip(input, (sum, first) => first + sum)
+                .ToArray();
+
+            return Solve1(sliding);
+        }
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day01Tests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day01Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[] {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+            var day1 = new Day01();
+
+            // Act
+            var result = day1.Solve1(input);
+
+            // Assert
+            result.Should().Be(7);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = ReadInput().ToArray();
+            var day1 = new Day01();
+
+            // Act
+            var result = day1.Solve1(input);
+
+            // Assert
+            result.Should().Be(1215);
+        }
+        
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[] {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+            var day1 = new Day01();
+
+            // Act
+            var result = day1.Solve2(input);
+
+            // Assert
+            result.Should().Be(5);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = ReadInput().ToArray();
+            var day1 = new Day01();
+
+            // Act
+            var result = day1.Solve2(input);
+
+            // Assert
+            result.Should().Be(1150);
+        }
+
+        private IEnumerable<int> ReadInput()
+        {
+            using var reader = File.OpenText("day01_puzzle01_input.txt");
+            var line = reader.ReadLine();
+            while (line is {Length: > 0})
+            {
+                yield return int.Parse(line);
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
