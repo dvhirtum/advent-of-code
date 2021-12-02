8a614606dd00e904df3c90fe88f839b278de0ca3
Dick van Hirtum <dick@xprtz.net>
2021-12-02 20:32:44 +0100

    day 02

---
+++ 2021/src/AdventOfCode.Puzzles/Day02.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day02
+    {
+        private readonly Regex _regex = new Regex(@"(?<direction>(forward|up|down)) (?<value>\d+)");
+
+        public int Solve1(string[] input)
+        {
+            var (position, depth) = input
+                .Where(x => _regex.IsMatch(x))
+                .Select(x => Command.Create(_regex.Match(x)))
+                .Select(x => x switch
+                {
+                    {Direction: "forward"} => (position: x.Value, depth: 0),
+                    {Direction: "down"} => (position: 0, depth: x.Value),
+                    {Direction: "up"} => (position: 0, depth: -x.Value),
+                    _ => throw new InvalidOperationException()
+                })
+                .Aggregate((acc, next) => (position: acc.position + next.position, depth: acc.depth + next.depth));
+
+            return position * depth;
+        }
+
+        public int Solve2(string[] input)
+        {
+            var (position, depth, _) = input
+                .Where(x => _regex.IsMatch(x))
+                .Select(x => Command.Create(_regex.Match(x)))
+                .Select(x => x switch
+                {
+                    {Direction: "forward"} => (position: x.Value, depth: 0, aim: 0),
+                    {Direction: "down"} => (position: 0, depth: 0, aim: x.Value),
+                    {Direction: "up"} => (position: 0, depth: 0, aim: -x.Value),
+                    _ => throw new InvalidOperationException()
+                })
+                .Aggregate((acc, next) =>
+                    (position: acc.position + next.position,
+                        depth: acc.depth + (acc.aim * next.position),
+                        aim: acc.aim + next.aim));
+
+            return position * depth;
+        }
+    }
+
+    public record Command(string Direction, int Value)
+    {
+        public static Command Create(Match match) => new(match.Groups["direction"].Value, int.Parse(match.Groups["value"].Value));
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day02Tests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day02Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[] {"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"};
+            var day02 = new Day02();
+            
+            // Act
+            var result = day02.Solve1(input);
+
+            // Assert
+            result.Should().Be(150);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = ReadInput().ToArray();
+            var day02 = new Day02();
+
+            // Act
+            var result = day02.Solve1(input);
+
+            // Assert
+            result.Should().Be(1990000);
+        }
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[] {"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"};
+            var day02 = new Day02();
+            
+            // Act
+            var result = day02.Solve2(input);
+
+            // Assert
+            result.Should().Be(900);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = ReadInput().ToArray();
+            var day02 = new Day02();
+
+            // Act
+            var result = day02.Solve2(input);
+
+            // Assert
+            result.Should().Be(1975421260);
+        }
+
+        private IEnumerable<string> ReadInput()
+        {
+            using var reader = File.OpenText("day02_input.txt");
+            var line = reader.ReadLine();
+            while (line is {Length: > 0})
+            {
+                yield return line;
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
