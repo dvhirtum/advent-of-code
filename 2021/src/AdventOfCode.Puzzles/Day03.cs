64ba1efec72f4a12d558ebab02cf3109ffeed0af
Dick van Hirtum <dick@xprtz.net>
2021-12-03 19:11:05 +0100

    day 3

---
+++ 2021/src/AdventOfCode.Puzzles/Day03.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day03
+    {
+        public int Solve1(string[] input)
+        {
+            var mostSignificantBits = input
+                .Select(x => x.Select(c => (int)char.GetNumericValue(c)))
+                .Aggregate((acc, next) => acc.Zip(next, (x, y) => x + (y == 0 ? -1 : 1)))
+                .Select(x => x > 0 ? 1 : 0)
+                .ToArray();
+            
+            var gammaRate = string.Join("", mostSignificantBits);
+            var epsilonRate = string.Join("", mostSignificantBits.Select(x => x == 1 ? 0 : 1));
+            
+            return Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+        }
+
+        public int Solve2(string[] input)
+        {
+            var oxygenGeneratorRating = OxygenGeneratorRating(input);
+            var co2ScrubberRating = Co2ScrubberRating(input);
+
+            return Convert.ToInt32(oxygenGeneratorRating, 2) * Convert.ToInt32(co2ScrubberRating, 2);
+        }
+
+        private string OxygenGeneratorRating(string[] input) 
+            => DetermineRating((a, b) => a.Length >= b.Length ? a : b, input, 0);
+
+        private string Co2ScrubberRating(string[] input) 
+            => DetermineRating((a, b) => a.Length >= b.Length ? b : a, input, 0);
+
+        private string DetermineRating(Func<string[], string[], string[]> selector, string[] input, int index)
+        {
+            if (input.Length == 1)
+            {
+                return input[0];
+            }
+
+            var bitIsSet = input.Where(x => x[index] == '1').ToArray();
+            var bitIsNotSet = input.Where(x => x[index]=='0').ToArray();
+
+            return DetermineRating(selector, selector(bitIsSet, bitIsNotSet), index + 1);
+        }
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day03Tests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day03Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[] {"00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"};
+            var day03 = new Day03();
+            
+            // Act
+            var result = day03.Solve1(input);
+
+            // Assert
+            result.Should().Be(198);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = ReadInput().ToArray();
+            var day03 = new Day03();
+
+            // Act
+            var result = day03.Solve1(input);
+
+            // Assert
+            result.Should().Be(3242606);
+        }
+
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[] {"00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"};
+            var day03 = new Day03();
+            
+            // Act
+            var result = day03.Solve2(input);
+
+            // Assert
+            result.Should().Be(230);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = ReadInput().ToArray();
+            var day03 = new Day03();
+
+            // Act
+            var result = day03.Solve2(input);
+
+            // Assert
+            result.Should().Be(4856080);
+        }
+        
+        private IEnumerable<string> ReadInput()
+        {
+            using var reader = File.OpenText("day03_input.txt");
+            var line = reader.ReadLine();
+            while (line is {Length: > 0})
+            {
+                yield return line;
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
