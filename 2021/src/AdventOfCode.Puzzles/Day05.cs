c209cbfe7635e81e61450072e17b42f6b762044a
Dick van Hirtum <dick@xprtz.net>
2021-12-05 17:51:42 +0100

    day 5

---
+++ 2021/src/AdventOfCode.Puzzles/Day05.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day05
+    {
+        private readonly Regex _regex = new(@"(?<x1>\d+),(?<y1>\d+) -> (?<x2>\d+),(?<y2>\d+)");
+        
+        public int Solve1(string[] input)
+        {
+            var points = new List<Point>();
+            
+            foreach (var line in input)
+            {
+                var match = _regex.Match(line);
+                var a = new Point(int.Parse(match.Groups["x1"].Value), int.Parse(match.Groups["y1"].Value));
+                var b = new Point(int.Parse(match.Groups["x2"].Value), int.Parse(match.Groups["y2"].Value));
+                
+                points.AddRange(a.PointsBetween(b));
+            }
+            
+            return points.GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .Select(x => x.Key)
+                .Distinct()
+                .Count();
+        }
+        
+        public int Solve2(string[] input)
+        {
+            var points = new List<Point>();
+            
+            foreach (var line in input)
+            {
+                var match = _regex.Match(line);
+                var a = new Point(int.Parse(match.Groups["x1"].Value), int.Parse(match.Groups["y1"].Value));
+                var b = new Point(int.Parse(match.Groups["x2"].Value), int.Parse(match.Groups["y2"].Value));
+                
+                points.AddRange(a.PointsBetweenIncludeDiagonal(b));
+            }
+            
+            return points.GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .Select(x => x.Key)
+                .Distinct()
+                .Count();
+        }
+    }
+
+    public record Point(int X, int Y)
+    {
+        public IEnumerable<Point> PointsBetween(Point point)
+        {
+            if (X == point.X)
+            {
+                var minY = Math.Min(Y, point.Y);
+                var maxY = Math.Max(Y, point.Y);
+                for (int i = minY; i <= maxY; i++)
+                {
+                    yield return new Point(X, i);
+                }
+            }
+            else if (Y == point.Y)
+            {
+                var minX = Math.Min(X, point.X);
+                var maxX = Math.Max(X, point.X);
+                for (int i = minX; i <= maxX; i++)
+                {
+                    yield return new Point(i, Y);
+                }
+            }
+        }
+        
+        public IEnumerable<Point> PointsBetweenIncludeDiagonal(Point point)
+        {
+            if (X == point.X)
+            {
+                var minY = Math.Min(Y, point.Y);
+                var maxY = Math.Max(Y, point.Y);
+                for (int i = minY; i <= maxY; i++)
+                {
+                    yield return new Point(X, i);
+                }
+            }
+            else if (Y == point.Y)
+            {
+                var minX = Math.Min(X, point.X);
+                var maxX = Math.Max(X, point.X);
+                for (int i = minX; i <= maxX; i++)
+                {
+                    yield return new Point(i, Y);
+                }
+            }
+            else
+            {
+                var minX = Math.Min(X, point.X);
+                var maxX = Math.Max(X, point.X);
+                var maxY = Math.Max(Y, point.Y);
+                var leftPoint = X > point.X ? point : this;
+                var dirY = leftPoint.Y == maxY ? -1 : 1;
+                for (int i = 0; i <= (maxX - minX); i++)
+                {
+                    yield return new Point(leftPoint.X + i, leftPoint.Y + (dirY * i));
+                }
+            }
+        }
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day05Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day05Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2"
+            };
+            var day05 = new Day05();
+            
+            // Act
+            var result = day05.Solve1(input);
+
+            // Assert
+            result.Should().Be(5);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day05").ToArray();
+            var day05 = new Day05();
+
+            // Act
+            var result = day05.Solve1(input);
+
+            // Assert
+            result.Should().Be(6856);
+        }
+        
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "0,9 -> 5,9",
+                "8,0 -> 0,8",
+                "9,4 -> 3,4",
+                "2,2 -> 2,1",
+                "7,0 -> 7,4",
+                "6,4 -> 2,0",
+                "0,9 -> 2,9",
+                "3,4 -> 1,4",
+                "0,0 -> 8,8",
+                "5,5 -> 8,2"
+            };
+            var day05 = new Day05();
+            
+            // Act
+            var result = day05.Solve2(input);
+
+            // Assert
+            result.Should().Be(12);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day05").ToArray();
+            var day05 = new Day05();
+
+            // Act
+            var result = day05.Solve2(input);
+
+            // Assert
+            result.Should().Be(6856);
+        }
+    }
+}
