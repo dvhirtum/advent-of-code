b4241c907a81e8be6f0444927d8b560aee812e8a
Dick van Hirtum <dick@xprtz.net>
2021-12-07 17:48:59 +0100

    day 7

---
+++ 2021/src/AdventOfCode.Puzzles/Day07.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day07
+    {
+        public int Solve1(int[] input) 
+            => Enumerable
+                .Range(input.Min(), input.Max() - input.Min())
+                .Min(i => input.Sum(x => Math.Abs(x - i)));
+
+        public int Solve2(int[] input) 
+            => Enumerable
+                .Range(input.Min(), input.Max() - input.Min())
+                .Min(i => input.Sum(x =>
+                {
+                    var n = Math.Abs(x - i);
+                    return n * (n + 1) / 2;
+                }));
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day07Tests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day07Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[] {16, 1, 2, 0, 4, 2, 7, 1, 2, 14};
+            var day07 = new Day07();
+
+            // Act
+            var result = day07.Solve1(input);
+
+            // Assert
+            result.Should().Be(37);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day07").ToArray();
+            var numbers = input[0].Split(",").Select(int.Parse).ToArray();
+            var day07 = new Day07();
+
+            // Act
+            var result = day07.Solve1(numbers);
+
+            // Assert
+            result.Should().Be(357353);
+        }
+        
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[] {16, 1, 2, 0, 4, 2, 7, 1, 2, 14};
+            var day07 = new Day07();
+
+            // Act
+            var result = day07.Solve2(input);
+
+            // Assert
+            result.Should().Be(168);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day07").ToArray();
+            var numbers = input[0].Split(",").Select(int.Parse).ToArray();
+            var day07 = new Day07();
+
+            // Act
+            var result = day07.Solve2(numbers);
+
+            // Assert
+            result.Should().Be(104822130);
+        }
+    }
+}
