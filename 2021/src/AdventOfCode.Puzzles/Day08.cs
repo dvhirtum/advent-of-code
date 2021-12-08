8b1921758be46b5fb98536ed05aef16ac9f8f88d
Dick van Hirtum <dick@xprtz.net>
2021-12-08 13:38:10 +0100

    day 8

---
+++ 2021/src/AdventOfCode.Puzzles/Day08.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day08
+    {
+        public int Solve1(string[] input)
+            => GetEntries(input).Sum(x => x.output.Count(n => n.Length is 2 or 3 or 4 or 7));
+
+        public int Solve2(string[] input) 
+            => GetEntries(input)
+                .Select(x =>
+                {
+                    var patternValues = MatchPatternsToValues(x);
+                    return (int)x.output
+                        .Select((o, i) => patternValues.Single(p => p.pattern == o).value * Math.Pow(10, 3 - i))
+                        .Sum();
+                }).Sum();
+
+        private List<(string pattern, int value)> MatchPatternsToValues((string[] patterns, string[] output) entry)
+        {
+            var patternValues = new List<(string pattern, int value)>
+            {
+                (entry.patterns.Single(p => p.Length == 2), 1),
+                (entry.patterns.Single(p => p.Length == 4), 4),
+                (entry.patterns.Single(p => p.Length == 3), 7),
+                (entry.patterns.Single(p => p.Length == 7), 8)
+            };
+
+            var nine = entry.patterns
+                .Single(p =>
+                    p.Length == 6
+                    && Regex.Matches(p, $"[{entry.patterns.Single(p1 => p1.Length == 4)}]").Count == 4);
+            patternValues.Add((nine, 9));
+
+            var zero = entry.patterns
+                .Single(p =>
+                    p.Length == 6
+                    && p != nine
+                    && Regex.Matches(p, $"[{entry.patterns.Single(p1 => p1.Length == 2)}]").Count == 2);
+            patternValues.Add((zero, 0));
+
+            var six = entry.patterns
+                .Single(p =>
+                    p.Length == 6
+                    && p != nine
+                    && p != zero);
+            patternValues.Add((six, 6));
+
+            var three = entry.patterns
+                .Single(p =>
+                    p.Length == 5
+                    && Regex.Matches(p, $"[{entry.patterns.Single(p1 => p1.Length == 2)}]").Count == 2);
+            patternValues.Add((three, 3));
+
+            var five = entry.patterns
+                .Single(p =>
+                    p.Length == 5
+                    && Regex.Matches(p, $"[{six}]").Count == 5);
+            patternValues.Add((five, 5));
+
+            var two = entry.patterns
+                .Single(p =>
+                    p.Length == 5
+                    && p != three
+                    && p != five);
+            patternValues.Add((two, 2));
+
+            return patternValues;
+        }
+
+        private IEnumerable<(string[] patterns, string[] output)> GetEntries(string[] input)
+            => input.Select(x =>
+            {
+                var split = x.Split(" | ");
+                return (
+                    patterns: split[0].Split(" ").Select(s => string.Concat(s.OrderBy(c => c))).ToArray(),
+                    output: split[1].Split(" ").Select(s => string.Concat(s.OrderBy(c => c))).ToArray()
+                );
+            });
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day08Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day08Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+                "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+                "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+                "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+                "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+                "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+                "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+                "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+                "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+                "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+            };
+            var day08 = new Day08();
+
+            // Act
+            var result = day08.Solve1(input);
+
+            // Assert
+            result.Should().Be(26);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day08").ToArray();
+            var day08 = new Day08();
+
+            // Act
+            var result = day08.Solve1(input);
+
+            // Assert
+            result.Should().Be(310);
+        }
+        
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+                "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+                "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+                "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+                "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+                "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+                "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+                "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+                "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+                "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+            };
+            var day08 = new Day08();
+
+            // Act
+            var result = day08.Solve2(input);
+
+            // Assert
+            result.Should().Be(61229);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day08").ToArray();
+            var day08 = new Day08();
+
+            // Act
+            var result = day08.Solve2(input);
+
+            // Assert
+            result.Should().Be(915941);
+        }
+    }
+}
