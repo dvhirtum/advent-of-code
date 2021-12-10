b09e785067b682331747349e7f8446952c5650f8
Dick van Hirtum <dick@xprtz.net>
2021-12-10 13:59:23 +0100

    day 10

---
+++ 2021/src/AdventOfCode.Puzzles/Day10.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Puzzles
+{
+    public class Day10
+    {
+        private readonly Dictionary<char, char> _matchingClosingChars =
+            new() {{'{', '}'}, {'[', ']'}, {'(', ')'}, {'<', '>'}};
+
+        private readonly Dictionary<char, char> _matchingOpeningChars =
+            new() {{'}', '{'}, {']', '['}, {')', '('}, {'>', '<'}};
+
+        public int Solve1(string[] input)
+            => input.Select(CheckForSyntaxError).Sum(x => x.score);
+
+        public long Solve2(string[] input)
+        {
+            var scores = input.Select(CheckForSyntaxError)
+                .Where(x => x.score == 0)
+                .Select(CompleteLineScore)
+                .OrderBy(x => x)
+                .ToArray();
+
+            return scores.Skip(scores.Length / 2).Take(1).Single();
+        }
+
+        private long CompleteLineScore((int score, Stack<char> incompleteLine) input)
+        {
+            var score = 0L;
+            while (input.incompleteLine.Any())
+            {
+                var character = input.incompleteLine.Pop();
+                score *= 5;
+                score += _matchingClosingChars[character] switch
+                {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+
+            return score;
+        }
+
+        private (int score, Stack<char> incompleteLine) CheckForSyntaxError(string input)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var character in input)
+            {
+                if (character is '{' or '(' or '[' or '<')
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    var previous = stack.Pop();
+
+                    if (previous != _matchingOpeningChars[character])
+                    {
+                        return character switch
+                        {
+                            ')' => (3, new Stack<char>()),
+                            ']' => (57, new Stack<char>()),
+                            '}' => (1197, new Stack<char>()),
+                            '>' => (25137, new Stack<char>()),
+                            _ => throw new ArgumentOutOfRangeException()
+                        };
+                    }
+                }
+            }
+
+            return (0, stack);
+        }
+    }
+}
---
+++ 2021/tests/AdventOfCode.Puzzles.UnitTests/Day10Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Puzzles.UnitTests
+{
+    public class Day10Tests
+    {
+        [Fact]
+        public void Solve1_Example()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "[({(<(())[]>[[{[]{<()<>>",
+                "[(()[<>])]({[<{<<[]>>(",
+                "{([(<{}[<>[]}>{[]{[(<()>",
+                "(((({<>}<{<{<>}{[]{[]{}",
+                "[[<[([]))<([[{}[[()]]]",
+                "[{[{({}]{}}([{[{{{}}([]",
+                "{<[[]]>}<{[{[{[]{()[[[]",
+                "[<(<(<(<{}))><([]([]()",
+                "<{([([[(<>()){}]>(<<{{",
+                "<{([{{}}[<[[[<>{}]]]>[]]"
+            };
+            var day10 = new Day10();
+
+            // Act
+            var result = day10.Solve1(input);
+
+            // Assert
+            result.Should().Be(26397);
+        }
+
+        [Fact]
+        public void Solve1_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day10").ToArray();
+            var day10 = new Day10();
+
+            // Act
+            var result = day10.Solve1(input);
+
+            // Assert
+            result.Should().Be(339537);
+        }
+        
+        [Fact]
+        public void Solve2_Example()
+        {
+            // Arrange
+            var input = new[]
+            {
+                "[({(<(())[]>[[{[]{<()<>>",
+                "[(()[<>])]({[<{<<[]>>(",
+                "{([(<{}[<>[]}>{[]{[(<()>",
+                "(((({<>}<{<{<>}{[]{[]{}",
+                "[[<[([]))<([[{}[[()]]]",
+                "[{[{({}]{}}([{[{{{}}([]",
+                "{<[[]]>}<{[{[{[]{()[[[]",
+                "[<(<(<(<{}))><([]([]()",
+                "<{([([[(<>()){}]>(<<{{",
+                "<{([{{}}[<[[[<>{}]]]>[]]"
+            };
+            var day10 = new Day10();
+
+            // Act
+            var result = day10.Solve2(input);
+
+            // Assert
+            result.Should().Be(288957L);
+        }
+
+        [Fact]
+        public void Solve2_Puzzle()
+        {
+            // Arrange
+            var input = InputReader.ReadInput("day10").ToArray();
+            var day10 = new Day10();
+
+            // Act
+            var result = day10.Solve2(input);
+
+            // Assert
+            result.Should().Be(2412013412L);
+        }
+    }
+}
